90d42ebee3ec8130e2238d3e35ab1cc539ea4e43
Ryan Brady <ryanbrady369@gmail.com>
2018-09-11 20:49:58 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,12 +4,12 @@
 def fizzbuzz(num)
   if num % 3 == 0
     if num % 5 == 0
-      return 'fizzbuzz'
+      'FizzBuzz'
     end
-    return 'fizz'
+    'Fizz'
   end
   if num % 5 == 0
-    return 'buzz'
+    'Buzz'
   end
   return nil
 end
